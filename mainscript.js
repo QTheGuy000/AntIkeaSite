153cac35a9346acb209305cbc4eca4f5a1b1600e
QTheGuy000 <157872344+QTheGuy000@users.noreply.github.com>
2024-05-08 22:17:30 -0700

    Add files via upload

---
+++ mainscript.js
@@ -0,0 +1,30 @@
+function change_page(){
+  window.location.href = "pitch.html";
+} 
+function change_page_back(){
+  window.location.href = "index.html";
+} 
+function Q1L() {
+  var x = document.getElementById("Q1");
+     x.innerHTML = "Incorrect!";
+} 
+function Q1W() {
+  var x = document.getElementById("Q1");
+     x.innerHTML = "Correct!";
+} 
+function Q2L() {
+  var x = document.getElementById("Q2");
+     x.innerHTML = "Incorrect!";
+} 
+function Q2W() {
+  var x = document.getElementById("Q2");
+     x.innerHTML = "Correct!";
+} 
+function Q3L() {
+  var x = document.getElementById("Q3");
+     x.innerHTML = "Incorrect!";
+} 
+function Q3W() {
+  var x = document.getElementById("Q3");
+     x.innerHTML = "Correct!";
+} 
